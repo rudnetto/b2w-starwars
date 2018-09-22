66bd97ade8a714acbfacc72832e41eba934660af
Rudney Netto <rudney.netto@concrete.com.br>
2018-09-22 04:33:32 -0300

    feat: add styled-components global style injection

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { injectGlobal } from 'styled-components';
+
+// eslint-disable-next-line
+injectGlobal`
+  body {
+    background-color: "white";
+    margin: 0;
+  }
+`;
 
 const App = () => (
   <span>Main component!</span>

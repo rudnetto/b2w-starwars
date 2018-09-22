5c24de41bf8688adc2b6cd9424298fddd5d24762
Rudney Netto <rudney.netto@concrete.com.br>
2018-09-22 04:27:42 -0300

    feat: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:flowtype/recommended'],
+  plugins: ['flowtype', 'compat'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'import/prefer-default-export': 1,
+    'implicit-arrow-linebreak': 0
+  },
+};

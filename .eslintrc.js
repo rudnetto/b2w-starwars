c550501ee822fa58bfc1616679751e69cf3b4067
Rudney Netto <rudneynetto@gmail.com>
2018-09-23 05:57:02 -0300

    chore: add react linting rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,11 @@ module.exports = {
   rules: {
     "strict": 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'react/destructuring-assignment': 0,
     'import/prefer-default-export': 1,
     'implicit-arrow-linebreak': 0,
     'linebreak-style': 0,
+    'operator-linebreak': 0,
+    'react/jsx-wrap-multilines': 0,
   },
 };

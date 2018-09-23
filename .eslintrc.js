6358d28f70f816ddd9472d73a7462ab391b8dfdb
Rudney Netto <rudneynetto@gmail.com>
2018-09-23 05:30:18 -0300

    chore: add linting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,10 @@ module.exports = {
     jest: true,
   },
   rules: {
+    "strict": 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
     'import/prefer-default-export': 1,
-    'implicit-arrow-linebreak': 0
+    'implicit-arrow-linebreak': 0,
+    'linebreak-style': 0,
   },
 };

77a4a8d362330205746e81b0440dae24eec69da7
Rudney Netto <rudneynetto@gmail.com>
2018-09-23 05:33:32 -0300

    feat: create service to fetch data from random planets

---
+++ src/modules/Planets/Service.js
@@ -0,0 +1,24 @@
+import HttpClient from '../../utils/HttpClient';
+import Random from '../../utils/Random';
+import { Planet } from './Models';
+
+const getRandomPlanet = async () => {
+  const number = Random.generateRandomNumber(1, 61);
+
+  const result = await HttpClient.get(`/planets/${number}`);
+  const planet: Planet = {
+    name: result.data.name,
+    climate: result.data.climate,
+    population: result.data.population,
+    terrain: result.data.terrain,
+    filmsFeatured: result.data.films.length,
+  };
+
+  return planet;
+}
+
+export default {
+  getRandomPlanet
+}
+
+

4eb4bc3d67c26495bdecd3dbf5c90f5ee089e2dc
Rudney Netto <rudneynetto@gmail.com>
2018-09-23 12:04:38 -0300

    chore: fix linting on test import

--- src/modules/App/App.spec.js
+++ src/modules/App/App.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import { shallow } from 'enzyme';
+import App from '.';
 
-it('renders without crashing', () => {
-  const element = shallow(<App />);
-  expect(element).toMatchSnapshot();
+describe('App test suite', () => {
+  it('renders without crashing', () => {
+    const element = shallow(<App />);
+    expect(element).toMatchSnapshot();
+  });
 });
